2eab7a552d5add9d79bd15e534c2abe55217e1b8
MBradley <mbradley@Steve-iLoop.local>
2012-12-07 11:53:45 -0500

    Initial Commit

---
+++ XCTimer/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  XCTimer
+//
+//  Created by MBradley on 12/7/12.
+//  Copyright (c) 2012 MBradley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
